be8ba373c7e4a3fb17716a53d18bb0089f2b3c89
Li Yixiao <157484964+223050025@users.noreply.github.com>
2024-10-22 10:41:22 +0800

    Update entrypoint.sh
    Signed-off-by: Li Yixiao <157484964+223050025@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,14 @@
 
 # working dir and output dir
 INPUT_DIR="/mnt/in"
-FASTQC_OUTPUT_DIR="/mnt/out/fastqc"
-MULTIQC_OUTPUT_DIR="/mnt/out/multiqc"
+OUTPUT_DIR="/mnt/out"
 
 # Source the script to set THREAD_NUM
 echo -e "\e[0;34mInfo: Running set.thread.num.sh to set THREAD_NUM...\e[0m"
 source /root/set.thread.num.sh
 
 # make-sure output dire
-mkdir -p "${FASTQC_OUTPUT_DIR}"
-mkdir -p "${MULTIQC_OUTPUT_DIR}"
+mkdir -p "${OUTPUT_DIR}"
 
 
 # find and fastqc every .fastq.gz
@@ -32,11 +30,11 @@ while IFS= read -r fqgz; do
     BASE_NAME=$(basename -- "$fqgz" .fastq.gz)
     echo -e "\e[0;34mInfo： Perform FastQC processing: ${BASE_NAME} ...\e[0m"
 
-    fastqc "$fqgz" -o "${FASTQC_OUTPUT_DIR}" --threads 4
+    fastqc "$fqgz" -o "${OUTPUT_DIR}" --threads $THREAD_NUM
     
     # Check whether the previous command was successful
     if [ $? -ne 0 ]; then
-        echo "Error running FastQC on $fqgz"
+        echo "Error running FastQC on $fqgz" >&2
         exit 1
     fi
 done < "$tmpfile"
@@ -44,13 +42,11 @@ rm "$tmpfile"
 
 # Run MultiQC on FastQC results
 echo -e "\e[0;34mInfo：Running MultiQC...\e[0m"
-multiqc "${FASTQC_OUTPUT_DIR}" -o "${MULTIQC_OUTPUT_DIR}"
+multiqc "${OUTPUT_DIR}" -o "${OUTPUT_DIR}"
 
 if [ $? -ne 0 ]; then
-    echo "!!!!!! Error running MultiQC"
-    echo -e "\e[0;31mError：MultiQC Fail\e[0m"
+    echo -e "\e[0;31mError：MultiQC Fail\e[0m" >&2
     exit 1
 else
-    echo "FastQC and MultiQC completed. Results are in ${MULTIQC_OUTPUT_DIR}"
-    echo -e "\e[0;34mInfo：FastQC and MultiQC completed. Results are in ${MULTIQC_OUTPUT_DIR}\e[0m"
+    echo -e "\e[0;34mInfo：FastQC and MultiQC completed. Results are in ${OUTPUT_DIR}\e[0m"
 fi
